79d8a8c402bb02d8fcaa951cbac5d445d7476b21
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-11-16 10:38:09 +0530

    :memo: Dynamic Wallet in Telegram

--- telegram/src/components/tma/tma-provider-initial-state.tsx
+++
@@ -1,9 +0,0 @@
-import { Logo } from "@/components/logo";
-
-export function TmaProviderInitialState() {
-  return (
-    <div>
-      <Logo className="w-12 h-12 text-primary-foreground" />
-    </div>
-  );
-}
